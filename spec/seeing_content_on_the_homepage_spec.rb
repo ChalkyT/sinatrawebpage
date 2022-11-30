004d1d9df17aaf19ba2fa6029848deceeea053cd
ChalkyT <rhian.cremer@madetech.com>
2022-11-30 10:39:15 +0000

    initial commit

---
+++ spec/seeing_content_on_the_homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'rack/test'
+require_relative '../hello_world'
+
+describe 'seeing content on the homepage' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('hello world!')
+  end
+end

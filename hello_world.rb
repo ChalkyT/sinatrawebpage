862bdb8afcc9a2de4dac33686e18fddca266b181
ChalkyT <rhian.cremer@madetech.com>
2022-12-16 14:50:27 +0000

    Additional CSV and Webpage Status

--- hello_world.rb
+++ hello_world.rb
@@ -11,7 +11,6 @@ get '/' do
         @api_data.select!.with_index{|data,index| index == 0 || data[2].downcase == @params[:area_name].downcase}
     end
 
-
     erb :index
 
 end
@@ -24,3 +23,24 @@ def results_counter()
     total_row_count = CSV.open('./mock_data - Sheet1.csv', 'r') { |row| row.count - 1}
 end
 
+def results_check()
+    total_row_count = results_counter()
+    if total_row_count < 1000
+        "This file has less than 1000 rows"
+    else
+        "*ALERT* This file has more than 1000 rows"
+    end
+end
+
+def check_each_row_same_num_columns()
+    CSV.foreach("./mock_data - Sheet1.csv") do |row|
+        col_length_test(row)
+    end
+"column number is always 6"
+end
+
+def col_length_test(row)
+    if row.length != 6
+        "*alert* there is a column number that is not 6"
+    end
+end

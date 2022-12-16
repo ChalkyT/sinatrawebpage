5739a37cb31a93fa32dad901d2fb343204ee04d6
ChalkyT <rhian.cremer@madetech.com>
2022-12-16 14:51:36 +0000

    Additional CSV data check functions

--- spec/seeing_content_on_the_homepage_spec.rb
+++ spec/seeing_content_on_the_homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rack/test'
 require_relative '../hello_world'
 
-describe 'seeing content on the homepage' do
+describe 'checking website is live' do
   include Rack::Test::Methods
 
   def app
@@ -12,18 +12,38 @@ describe 'seeing content on the homepage' do
     get '/'
     expect(last_response).to be_ok
   end
+
+  it "checks the HTTP status is 200" do
+    get '/'
+    expect(last_response.status).to eq(200)
+  end
 end
 
 describe 'CSV data tests' do
   it 'returns number of columns in mock_data - Sheet1.csv as 6' do
       expect(check_num_columns()).to eq(6)
   end
+
+  it 'returns a message if the file is less than 1000 rows' do
+    expect(results_check()).to eq("This file has less than 1000 rows")
+  end
+
+  xit 'returns a message if the file has more than 1000 rows' do
+    expect(results_check()).to eq("*ALERT* This file has more than 1000 rows")
+  end
+
+  it 'checks each row for same number of columns to equal 6' do
+    expect(check_each_row_same_num_columns()).to eq("column number is always 6")
+  end
+
+  it 'checks each row for same number of columns and if not 6 returns error message' do
+    expect(col_length_test("test,test,test")).to eq("*alert* there is a column number that is not 6")
+  end
 end
 
 describe 'Output to website' do
-  it 'returns 100 results' do
+  it 'returns the number of rows in the CSV file except for the header row' do
       expect(results_counter()).to eq(100)
   end
 
-  
 end
